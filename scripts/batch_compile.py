0f1d2cf68e4f5e644a8b3d00ecf6ec7d03b56f0d
Henry Yu <hnryu@ucdavis.edu>
2022-12-13 23:22:02 +0000

    feat(scripts): add help messages

--- scripts/batch_compile.py
+++ scripts/batch_compile.py
@@ -80,11 +80,36 @@ def main() -> None:
         help="The number of concurrent subprocesses",
     )
 
+    parser.add_argument(
+        "--csmith-root",
+        type=str,
+        default="../csmith",
+        help="The root directory for CSmith repo",
+    )
+
     args = parser.parse_args()
 
     def batch_compile_wrapper(
-        target: str, sysroot: Optional[str] = None, include: Optional[str] = None
+        target: str,
+        sysroot: Optional[str] = None,
+        include: Optional[str] = None,
+        apt_package: Optional[str] = None,
+        link: Optional[str] = None,
     ) -> None:
+        if not os.path.exists(args.csmith_root):
+            print(f"ERROR: missing CSmith in {args.csmith_root}.")
+            print(f"Run `git clone https://github.com/csmith-project/csmith.git {args.csmith_root}`")
+
+        if (include is not None and not os.path.exists(include)) or (
+            sysroot is not None and not os.path.exists(sysroot)
+        ):
+            print(f"ERROR: missing headers for target {target}.")
+            if apt_package is not None:
+                print(f"Run `sudo apt install {apt_package}`.")
+            if link is not None:
+                print(f"See {link} for how to get the required headers.")
+            return
+
         batch_compile(
             src_dir=args.input,
             out_dir=os.path.join(args.output, target),
@@ -92,7 +117,7 @@ def main() -> None:
                 build_clang_flags(
                     target=target,
                     sysroot=sysroot,
-                    include_paths=["../csmith/runtime/"]
+                    include_paths=[os.path.join(args.csmith_root, "runtime")]
                     + ([] if include is None else [include]),
                     opt_level="2",
                 )
@@ -100,13 +125,41 @@ def main() -> None:
             n_jobs=args.jobs,
         )
 
-    batch_compile_wrapper("i686", include="/usr/i686-linux-gnu/include")
-    batch_compile_wrapper("x86_64", include="/usr/x86_64-linux-gnu/include")
-    batch_compile_wrapper("arm", include="/usr/arm-linux-gnueabi/include")
-    batch_compile_wrapper("aarch64", include="/usr/aarch64-linux-gnu/include")
-    batch_compile_wrapper("riscv32", sysroot="./riscv32/sysroot")
-    batch_compile_wrapper("riscv64", include="/usr/riscv64-linux-gnu/include")
-    batch_compile_wrapper("wasm32-wasi", sysroot="./wasi-sdk-14.0/share/wasi-sysroot")
+    batch_compile_wrapper(
+        "i686",
+        include="/usr/i686-linux-gnu/include",
+        apt_package="libc6-dev-i386-cross",
+    )
+    batch_compile_wrapper(
+        "x86_64",
+        include="/usr/x86_64-linux-gnu/include",
+        apt_package="libc6-dev-amd64-cross",
+    )
+    batch_compile_wrapper(
+        "arm",
+        include="/usr/arm-linux-gnueabi/include",
+        apt_package="libc6-dev-armel-cross",
+    )
+    batch_compile_wrapper(
+        "aarch64",
+        include="/usr/aarch64-linux-gnu/include",
+        apt_package="libc6-dev-arm64-cross",
+    )
+    batch_compile_wrapper(
+        "riscv32",
+        include="./riscv32/sysroot/usr/include",
+        link="https://github.com/riscv-collab/riscv-gnu-toolchain",
+    )
+    batch_compile_wrapper(
+        "riscv64",
+        include="/usr/riscv64-linux-gnu/include",
+        apt_package="libc6-dev-riscv64-cross",
+    )
+    batch_compile_wrapper(
+        "wasm32-wasi",
+        sysroot="./wasi-sdk-14.0/share/wasi-sysroot",
+        link="https://github.com/WebAssembly/wasi-sdk",
+    )
 
 
 if __name__ == "__main__":

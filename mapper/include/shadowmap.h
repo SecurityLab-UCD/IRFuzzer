ae4742ab846f0f222b927974c7a6af233fcb1ca4
Zhenkai Weng <theoaktree1040@gmail.com>
2023-05-10 16:03:20 -0700

    feat(mapper): Print map stats in diff and intersect

--- mapper/include/shadowmap.h
+++ mapper/include/shadowmap.h
@@ -12,7 +12,7 @@ std::vector<bool> readShadowMap(size_t MapBitSize, const std::string &FileName);
 bool writeShadowMap(const std::vector<bool> &Map, const std::string &FileName);
 
 void printShadowMapStats(const std::vector<bool> &ShadowMap,
-                         const std::string &Description = "Upper bound",
+                         const std::string &Description,
                          const std::string &FileName = "");
 
 #endif // SHADOW_MAP_H_

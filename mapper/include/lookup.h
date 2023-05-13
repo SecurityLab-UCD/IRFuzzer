2031d8317e8b91debe5d6f6c4e84da15084685e2
Zhenkai Weng <theoaktree1040@gmail.com>
2023-05-12 20:39:14 -0700

    feat(mapper): More accurate upper bound
    Previously the upper bound calculation assumes that only MorphToNode and
    CompleteMatch matchers are failed by unsatisfied predicates, which is of
    course grossly inaccurate, since SelectCodeCommon() actually exits the
    current scope if a pattern predicate check fails, which means nodes such
    as MorphToNode would never ever be reached if a predicate fails.
    
    The new upper bound calculation mimics SelectCodeCommon() more closely
    by using the same early-exit method when accessing the matcher table.
    The new upper bound calculations are much lower (as they should be).
    
    Known issues:
    - Some predicate literals parsed from pattern predicate expressions are
      not found in the original list of predicates. Some of them may be
      dependent on the information of the current function, which can be
      identified by checking the RecomputePerFunction flag in their TableGen
      record (see `jq .predicates lookup.json | grep "RecomputePerFunction =
      1"`).
    - In the case of unknown literal mentioned above, it may be a better
      idea to exclude from pattern checks completely instead of setting them
      to true or false. However, to actually make the pattern predicates
      behave correctly, we have to manually insert corresponding literals
      into the "predicates" key of the pattern lookup table.

--- mapper/include/lookup.h
+++ mapper/include/lookup.h
@@ -2,40 +2,97 @@
 #ifndef LOOKUP_H_
 #define LOOKUP_H_
 
+#include "predicate.h"
+#include "llvm/ADT/MapVector.h"
 #include "llvm/ADT/SmallVector.h"
 #include "llvm/ADT/StringRef.h"
 #include "llvm/Support/JSON.h"
+#include <map>
 #include <string>
 
 struct Matcher {
+
+  // Matcher kind borrowed from DAGISelMatcher.h
+  // Keep updated with llvm's version
+  enum KindTy {
+    // Matcher state manipulation.
+    Scope,            // Push a checking scope.
+    RecordNode,       // Record the current node.
+    RecordChild,      // Record a child of the current node.
+    RecordMemRef,     // Record the memref in the current node.
+    CaptureGlueInput, // If the current node has an input glue, save it.
+    MoveChild,        // Move current node to specified child.
+    MoveParent,       // Move current node to parent.
+
+    // Predicate checking.
+    CheckSame,             // Fail if not same as prev match.
+    CheckChildSame,        // Fail if child not same as prev match.
+    CheckPatternPredicate, //
+    CheckPredicate,        // Fail if node predicate fails.
+    CheckOpcode,           // Fail if not opcode.
+    SwitchOpcode,          // Dispatch based on opcode.
+    CheckType,             // Fail if not correct type.
+    SwitchType,            // Dispatch based on type.
+    CheckChildType,        // Fail if child has wrong type.
+    CheckInteger,          // Fail if wrong val.
+    CheckChildInteger,     // Fail if child is wrong val.
+    CheckCondCode,         // Fail if not condcode.
+    CheckChild2CondCode,   // Fail if child is wrong condcode.
+    CheckValueType,
+    CheckComplexPat,
+    CheckAndImm,
+    CheckOrImm,
+    CheckImmAllOnesV,
+    CheckImmAllZerosV,
+    CheckFoldableChainNode,
+
+    // Node creation/emisssion.
+    EmitInteger,          // Create a TargetConstant
+    EmitStringInteger,    // Create a TargetConstant from a string.
+    EmitRegister,         // Create a register.
+    EmitConvertToTarget,  // Convert a imm/fpimm to target imm/fpimm
+    EmitMergeInputChains, // Merge together a chains for an input.
+    EmitCopyToReg,        // Emit a copytoreg into a physreg.
+    EmitNode,             // Create a DAG node
+    EmitNodeXForm,        // Run a SDNodeXForm
+    CompleteMatch,        // Finish a match and update the results.
+    MorphNodeTo,          // Build a node, finish a match and update results.
+
+    // Highest enum value; watch out when adding more.
+    HighestKind = MorphNodeTo
+  };
+
   size_t Idx;
   size_t Size;
-  int Kind; // Matcher::KindTy enum
-  size_t PatternIdx;
+  KindTy Kind;
+  std::optional<size_t> PatternIdx;
+  std::optional<size_t> PatPredIdx;
 
-  Matcher() : Idx(0), Size(0), Kind(0), PatternIdx(0) {}
+  Matcher() : Idx(), Size(), Kind() {}
   bool operator<(const Matcher &M) const;
   bool hasPattern() const {
-    // NOTE: These values must match the enum values of Matcher::CompleteMatch
-    // and Matcher::MorphNodeTo. DAG ISel Matcher::KindTy is defined in
-    // llvm/utils/TableGen which we don't have access to.
-    return Kind == 35 || Kind == 36;
+    return Kind == CompleteMatch || Kind == MorphNodeTo;
   }
 };
 
 struct Pattern {
-  llvm::StringRef IncludePath;
-  llvm::StringRef PatternSrc;
-  llvm::SmallVector<size_t, 3> Predicates;
+  std::string IncludePath;
+  std::string PatternSrc;
+  llvm::SmallVector<size_t, 3> NamedPredicates;
+
+  // Index of this pattern in lookup table (for debugging purposes)
+  size_t Index;
 };
 
-llvm::Expected<llvm::json::Value>
-parseLookupTable(const std::string &LookupFilename);
+struct LookupTable {
+  std::vector<Pattern> Patterns;
+  // Always sorted by index
+  std::vector<Matcher> Matchers;
+  size_t MatcherTableSize;
+  PredicateKeeper PK;
 
-std::vector<llvm::StringRef>
-getPredicates(const llvm::json::Object &LookupTable);
+  static LookupTable fromFile(const std::string &Filename,
+                              bool NameCaseSensitive = false);
+};
 
-std::vector<Pattern> getPatterns(const llvm::json::Object &LookupTable);
-
-std::vector<Matcher> getMatchers(const llvm::json::Object &LookupTable);
 #endif // LOOKUP_H_

9f351f956a2f31d172d86158a2a70c3f8d3b4fcb
Zhenkai Weng <theoaktree1040@gmail.com>
2023-05-13 19:56:05 -0700

    feat(mapper): Pretty table printing

--- mapper/include/shadowmap.h
+++ mapper/include/shadowmap.h
@@ -1,43 +1,71 @@
 #pragma once
 #ifndef SHADOW_MAP_H_
 #define SHADOW_MAP_H_
+#include "llvm/Support/raw_ostream.h"
 #include <functional>
+#include <iomanip>
+#include <limits>
+#include <sstream>
 #include <string>
 #include <vector>
 
 std::vector<bool> readShadowMap(size_t MapBitSize, const std::string &FileName);
 
+std::vector<std::vector<bool>>
+readShadowMaps(size_t MapBitSize, const std::vector<std::string> &FileNames);
+
 bool writeShadowMap(const std::vector<bool> &Map, const std::string &FileName);
 
-size_t getCoveredIndices(const std::vector<bool> &Map);
+std::vector<bool> doMapOp(const std::vector<std::vector<bool>> &Maps,
+                          std::function<bool(bool, bool)> Op);
 
-void printShadowMapStats(const std::vector<bool> &ShadowMap,
-                         const std::string &Description,
-                         const std::string &FileName = "");
+class MapStatPrinter {
+  typedef std::tuple<std::string, std::string, size_t, size_t> StatTy;
 
-void printShadowMapStats(size_t CoveredIndices, size_t MapSize,
-                         const std::string &Description,
-                         const std::string &FileName = "");
+  std::vector<StatTy> Stats;
+  size_t MaxFilenameLen = 0;
+  size_t MaxTableSize = 0;
+  std::string Description = "";
+  size_t MaxDescLen = 0;
+  mutable size_t Limit = std::numeric_limits<size_t>::max();
 
-template <typename ConstIterator>
-std::vector<bool> doMapOp(size_t TableSize, ConstIterator MapFilesBegin,
-                          ConstIterator MapFilesEnd,
-                          std::function<bool(bool, bool)> Op,
-                          bool PrintStats = false) {
-  std::vector<std::vector<bool>> Maps;
-  for (auto It = MapFilesBegin; It != MapFilesEnd; It++) {
-    const std::string &MapFile = *It;
-    Maps.push_back(readShadowMap(TableSize, MapFile));
-    if (PrintStats)
-      printShadowMapStats(*Maps.rbegin(), "", MapFile);
+public:
+  enum SortTy { None, Asc, Desc };
+
+  // Set row description
+  MapStatPrinter(const std::string &Description)
+      : Description(Description), MaxDescLen(Description.size()) {}
+  // No description
+  MapStatPrinter() = default;
+
+  static inline size_t getIdxCovered(const std::vector<bool> &Map) {
+    return std::count(Map.begin(), Map.end(), false);
   }
-  std::vector<bool> ResultMap(Maps[0]);
-  for (size_t M = 1; M < Maps.size(); M++) {
-    for (size_t I = 0; I < TableSize; I++) {
-      ResultMap[I] = Op(ResultMap[I], Maps[M][I]);
-    }
-  }
-  return ResultMap;
-}
+
+  std::string format(const StatTy &Stat) const;
+  void print() const;
+
+  void addFile(const std::string &Filename, const std::vector<bool> &Map);
+  void addFile(const std::string &Filename, size_t Covered, size_t TableSize);
+  void addFile(const std::string &Filename, size_t TableSize);
+  void addStat(size_t Covered, size_t TableSize);
+  void addStat(const std::string &Desc, size_t Covered, size_t TableSize);
+  void addMap(const std::vector<bool> &Map);
+  void summarize(const std::string &Desc, size_t Covered, size_t TableSize,
+                 bool alignToDesc = false);
+  void summarize(const std::string &Desc, const std::vector<bool> &Map,
+                 bool alignToDesc = false);
+
+  void asc();
+  void desc();
+  void sort(SortTy S);
+
+  void limit(size_t L);
+  bool atLimit() const;
+
+private:
+  void addStat(const std::string &Filename, const std::string &Desc,
+               size_t Covered, size_t TableSize);
+};
 
 #endif // SHADOW_MAP_H_
--- mapper/src/shadowmap.cpp
+++ mapper/src/shadowmap.cpp
@@ -38,6 +38,15 @@ std::vector<bool> readShadowMap(size_t MapBitSize,
   return ShadowMap;
 }
 
+std::vector<std::vector<bool>>
+readShadowMaps(size_t MapBitSize, const std::vector<std::string> &FileNames) {
+  std::vector<std::vector<bool>> Result;
+  for (const auto &FileName : FileNames) {
+    Result.push_back(readShadowMap(MapBitSize, FileName));
+  }
+  return Result;
+}
+
 bool writeShadowMap(const std::vector<bool> &Map, const std::string &FileName) {
   std::ofstream ShadowMapOfs(FileName, std::ios::binary);
   if (!ShadowMapOfs)
@@ -58,35 +67,133 @@ bool writeShadowMap(const std::vector<bool> &Map, const std::string &FileName) {
   return ShadowMapOfs.good();
 }
 
-size_t getCoveredIndices(const std::vector<bool> &Map) {
-  return std::count(Map.begin(), Map.end(), false);
+std::vector<bool> doMapOp(const std::vector<std::vector<bool>> &Maps,
+                          std::function<bool(bool, bool)> Op) {
+  if (Maps.empty())
+    return std::vector<bool>();
+  std::vector<bool> ResultMap(Maps[0]);
+  for (size_t M = 1; M < Maps.size(); M++) {
+    for (size_t I = 0; I < Maps[0].size(); I++) {
+      ResultMap[I] = Op(ResultMap[I], Maps[M][I]);
+    }
+  }
+  return ResultMap;
 }
 
-void printShadowMapStats(const std::vector<bool> &ShadowMap,
-                         const std::string &Description,
-                         const std::string &FileName) {
-  printShadowMapStats(getCoveredIndices(ShadowMap), ShadowMap.size(),
-                      Description, FileName);
-}
+std::string MapStatPrinter::format(const MapStatPrinter::StatTy &Stat) const {
+  const auto &[Filename, Desc, Covered, TableSize] = Stat;
+  std::stringstream Result;
+  Result << std::setw(MaxFilenameLen) << Filename;
+  if (Filename.size())
+    Result << ": ";
+  else if (MaxFilenameLen)
+    Result << "  ";
 
-void printShadowMapStats(size_t CoveredIndices, size_t MapSize,
-                         const std::string &Description,
-                         const std::string &FileName) {
-  double Coverage = CoveredIndices;
-  Coverage /= MapSize;
+  Result << std::setw(MaxDescLen) << Desc;
+  if (Desc.size())
+    Result << ": ";
+  else if (MaxDescLen)
+    Result << "  ";
+
+  size_t IdxWidth = std::to_string(MaxTableSize).size();
+  Result << std::setw(IdxWidth) << Covered << " out of ";
+  Result << std::setw(IdxWidth) << TableSize;
+
+  double Coverage = Covered;
+  Coverage /= TableSize;
   Coverage *= 100;
 
-  if (!FileName.empty()) {
-    llvm::outs() << "[" << FileName << "] ";
-  }
-  if (!Description.empty()) {
-    llvm::outs() << Description << ": ";
-  }
+  Result << " (" << std::to_string(Coverage) << "%)";
+  return Result.str();
+}
 
-  std::string CIStr = std::to_string(CoveredIndices);
-  size_t PadLen = std::to_string(MapSize).size();
-  CIStr.insert(CIStr.begin(), PadLen - CIStr.size(), ' ');
-  llvm::outs() << CIStr << " out of " << MapSize << " ("
-               << std::to_string(Coverage) << "%)";
-  llvm::outs() << '\n';
+void MapStatPrinter::print() const {
+  for (const StatTy &Stat : Stats) {
+    llvm::outs() << format(Stat) << '\n';
+  }
+}
+
+void MapStatPrinter::addFile(const std::string &Filename,
+                             const std::vector<bool> &Map) {
+  addStat(Filename, Description, getIdxCovered(Map), Map.size());
+}
+
+void MapStatPrinter::addFile(const std::string &Filename, size_t Covered,
+                             size_t TableSize) {
+  addStat(Filename, Description, Covered, TableSize);
+}
+
+void MapStatPrinter::addFile(const std::string &Filename, size_t TableSize) {
+  std::vector<bool> Map = readShadowMap(TableSize, Filename);
+  addFile(Filename, getIdxCovered(Map), TableSize);
+}
+
+void MapStatPrinter::addStat(size_t Covered, size_t TableSize) {
+  addStat("", Description, Covered, TableSize);
+}
+
+void MapStatPrinter::addStat(const std::string &Desc, size_t Covered,
+                             size_t TableSize) {
+  addStat("", Desc, Covered, TableSize);
+}
+
+void MapStatPrinter::addMap(const std::vector<bool> &Map) {
+  addStat("", Description, getIdxCovered(Map), Map.size());
+}
+
+void MapStatPrinter::summarize(const std::string &Desc, size_t Covered,
+                               size_t TableSize, bool alignToDesc) {
+  if (Limit < std::numeric_limits<size_t>::max())
+    Limit++;
+  if (alignToDesc) {
+    addStat("", Desc, Covered, TableSize);
+  } else {
+    addStat(Desc, "", Covered, TableSize);
+  }
+}
+
+void MapStatPrinter::summarize(const std::string &Desc,
+                               const std::vector<bool> &Map, bool alignToDesc) {
+  size_t Covered = getIdxCovered(Map);
+  summarize(Desc, Covered, Map.size(), alignToDesc);
+}
+
+void MapStatPrinter::asc() {
+  std::sort(Stats.begin(), Stats.end(), [](const auto &a, const auto &b) {
+    return std::get<2>(a) < std::get<2>(b);
+  });
+}
+
+void MapStatPrinter::desc() {
+  std::sort(Stats.begin(), Stats.end(), [](const auto &a, const auto &b) {
+    return std::get<2>(a) > std::get<2>(b);
+  });
+}
+
+void MapStatPrinter::sort(SortTy S) {
+  switch (S) {
+  default:
+    break;
+  case Asc:
+    asc();
+    break;
+  case Desc:
+    desc();
+    break;
+  }
+}
+
+void MapStatPrinter::limit(size_t L) { Limit = L; }
+bool MapStatPrinter::atLimit() const { return Limit == 0; }
+
+void MapStatPrinter::addStat(const std::string &Filename,
+                             const std::string &Desc, size_t Covered,
+                             size_t TableSize) {
+  if (Limit == 0)
+    return;
+  Limit--;
+  MaxTableSize = std::max(MaxTableSize, TableSize);
+  MaxFilenameLen = std::max(MaxFilenameLen, Filename.size());
+  MaxDescLen = std::max(MaxDescLen, Desc.size());
+  Stats.push_back(std::tuple(Filename, Desc, Covered, TableSize));
 }

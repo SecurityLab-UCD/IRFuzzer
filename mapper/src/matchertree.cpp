9f22e522b682b002ffd2b502e5ac0c6db70370c0
Zhenkai Weng <theoaktree1040@gmail.com>
2023-05-14 00:50:29 -0700

    fix(mapper): Do not create actual tree in MatcherTree

--- mapper/src/matchertree.cpp
+++ mapper/src/matchertree.cpp
@@ -5,45 +5,6 @@
 
 using namespace llvm;
 
-bool MatcherNode::Overlaps(const MatcherNode &N) const {
-  // make a the left-most interval
-  const MatcherNode &A = M.Begin <= N.M.Begin ? *this : N;
-  const MatcherNode &B = M.Begin <= N.M.Begin ? N : *this;
-
-  return A != B && !A.Contains(B) && !B.Contains(A) && A.M.End <= B.M.Begin;
-}
-
-MatcherTree::MatcherTree(const LookupTable &_LT) : Root(nullptr), LT(_LT) {
-  for (const Matcher &M : LT.Matchers) {
-    insert(M);
-  }
-}
-
-void MatcherTree::insert(const Matcher &M) {
-  MatcherNode *NewNode = new MatcherNode(M);
-  if (!Root) {
-    Root = NewNode;
-    return;
-  }
-  MatcherNode *Node = Root;
-  while (Node->Children.size() > 0) {
-    bool isChildFound = false;
-    for (MatcherNode *Child : Node->Children) {
-      if (Child->Contains(*NewNode)) {
-        Node = Child;
-        isChildFound = true;
-        break;
-      }
-    }
-    if (!isChildFound)
-      break;
-  }
-  if (!Node->Contains(*NewNode)) {
-    llvm::report_fatal_error("Insertion interval does not encompass new node.");
-  }
-  Node->Children.push_back(NewNode);
-}
-
 template <typename A, typename B>
 std::pair<B, A> FlipPair(const std::pair<A, B> &p) {
   return std::pair<B, A>(p.second, p.first);
@@ -63,63 +24,80 @@ std::multimap<B, A, std::greater<B>> FlipMap(const M<A, B, Args...> &Src) {
 std::tuple<size_t, std::vector<bool>,
            std::multimap<size_t, size_t, std::greater<size_t>>>
 MatcherTree::getUpperBound() const {
-  if (!Root)
+  if (MT.empty())
     return std::tuple(0, std::vector<bool>(),
                       std::multimap<size_t, size_t, std::greater<size_t>>());
-  std::vector<bool> ShadowMap(Root->M.Size());
-  size_t UpperBound = Root->M.Size();
+  std::vector<bool> ShadowMap(MT[0].Size());
+  size_t UpperBound = ShadowMap.size();
   std::unordered_map<size_t, size_t> BlameMap;
-  visit(Root, UpperBound, ShadowMap, BlameMap);
+  size_t I = 0;
+  visit(I, UpperBound, ShadowMap, BlameMap);
   return std::tuple(UpperBound, ShadowMap, FlipMap(BlameMap));
 }
 
-void MatcherTree::visit(MatcherNode *N, size_t &UpperBound,
+/// @brief Visit a matcher tree node
+/// @param I current matcher tree index
+/// @param UpperBound Current upper bound value
+/// @param ShadowMap Shadow map
+/// @param BlameMap Pattern predicate blame list (pat pred -> loss)
+/// @return if this leaf failed a pattern predicate check (not named predicate)
+bool MatcherTree::visit(size_t &I, size_t &UpperBound,
                         std::vector<bool> &ShadowMap,
                         std::unordered_map<size_t, size_t> &BlameMap) const {
-  if (N->Children.size() == 0) {
-    if (!LT.PK.Verbosity)
-      return;
-    if (!N->M.hasPattern())
-      return;
-    const Pattern &Pat = LT.Patterns[N->M.PIdx];
-    for (size_t Pred : Pat.NamedPredicates) {
-      // Supposedly this named predicate was checked as part of the
-      // pattern predicate.
-      if (!LT.PK.name(Pred)->satisfied()) {
-        // This is bad.
-        errs() << "ERROR: Failed named predicate check " << Pred << " at "
-               << N->M.Begin << ".\n";
-        errs() << "ERROR: Reached leaf when named predicate is unsatisfied!\n";
+  if (MT[I].isLeaf()) {
+    // We only care about leaves with a pattern or pattern predicate index
+    if (MT[I].hasPattern() && LT.PK.Verbosity) {
+      const Pattern &Pat = LT.Patterns[MT[I].PIdx];
+      // Verify that all named predicates are satisfied.
+      for (size_t Pred : Pat.NamedPredicates) {
+        if (!LT.PK.name(Pred)->satisfied()) {
+          // In certain cases, some matchers have the same TableGen pattern but
+          // different predicates, and the one with a different predicate is not
+          // captured. They don't really cause an issue for the calculation, so
+          // we just emit an error message and move on.
+          errs()
+              << "ERROR: Failed named predicate check " << Pred << " at "
+              << MT[I].Begin << ".\n"
+              << "ERROR: Reached leaf when named predicate is unsatisfied!\n";
+        }
+      }
+    } else if (MT[I].hasPatPred()) {
+      if (!LT.PK.pat(MT[I].PIdx)->satisfied()) {
+        I++;
+        return true;
+      } else if (LT.PK.Verbosity > 2) {
+        dbgs() << "DEBUG: Passed pattern predicate check " << MT[I].PIdx
+               << " at " << MT[I].Begin << ".\n";
       }
     }
-    return;
+    I++;
+    return false;
   }
 
-  size_t FailedIndex = N->M.End + 1;
-  for (MatcherNode *C : N->Children) {
-    if (LT.PK.Verbosity > 2 && C->M.hasPatPred() &&
-        LT.PK.pat(C->M.PIdx)->satisfied()) {
-      dbgs() << "DEBUG: Passed pattern predicate check " << C->M.PIdx << " at "
-             << C->M.Begin << ".\n";
-    }
-    if (C->M.hasPatPred() && !LT.PK.pat(C->M.PIdx)->satisfied()) {
-      FailedIndex = C->M.End + 1;
+  // We have a switch, scope, or a child (i.e. switch or scope case)
+  size_t PI = I; // parent index
+  I++;
+  bool Failed = false;
+  for (; I < MT.size() && MT[PI].Contains(MT[I]);) {
+    if (!Failed) {
+      Failed = visit(I, UpperBound, ShadowMap, BlameMap);
+    } else { // Previous pattern predicate check failed
+      size_t Loss = MT[PI].End - MT[I].Begin + 1;
+      BlameMap[MT[I].PIdx] += Loss;
+      UpperBound -= Loss;
+
       if (LT.PK.Verbosity > 2)
-        errs() << "DEBUG: Failed pattern predicate check " << C->M.PIdx
-               << " at " << C->M.Begin << " (-" << (N->M.End - FailedIndex + 1)
-               << ").\n";
-      BlameMap[C->M.PIdx] += N->M.End - FailedIndex + 1;
-      // CheckPatternPredicate is accessed, but not the rest
-      UpperBound -= N->M.End - FailedIndex + 1;
-      for (size_t I = FailedIndex; I <= N->M.End; I++) {
-        if (LT.PK.Verbosity && ShadowMap[I]) {
-          dbgs() << "ERROR: Shadow map at i=" << I
-                 << " is already set to true.\n";
-        }
-        ShadowMap[I] = true;
-      }
-      break;
+        errs() << "DEBUG: Failed pattern predicate check " << MT[I - 1].PIdx
+               << " at " << MT[I - 1].Begin << " (-" << Loss << ").\n";
+
+      std::fill(ShadowMap.begin() + MT[I].Begin,
+                ShadowMap.begin() + MT[PI].End + 1, true);
+
+      // Fast-forward out of the parent
+      while (I < MT.size() && MT[PI].Contains(MT[I]))
+        I++;
+      return false;
     }
-    visit(C, UpperBound, ShadowMap, BlameMap);
   }
+  return false;
 }

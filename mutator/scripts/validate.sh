e492aaccd44c9b508aa9858e35129cf0ed54ca4a
Peter Rong <peterrong96@gmail.com>
2023-06-07 15:35:19 -0700

    adding casting and verified (#39)
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>

--- mutator/scripts/validate.sh
+++ mutator/scripts/validate.sh
@@ -1,19 +1,21 @@
 INPUT=$1
+NPROC=$2
 
-if [[ -z $INPUT ]]
-then
+if [[ -z $INPUT ]]; then
     INPUT=$FUZZING_HOME/seeds/seed.bc
 fi
 
-export NPROC=`nproc --all`
-export NPROC=1
+if [[ -z $NPROC ]]; then
+    export NPROC=`nproc --all`
+fi
+
 for J in $(seq $NPROC)
 do
     rm O$J; touch O$J
     for I in $(seq 1000)
     do     
         SEED=$(shuf -i 0-4294967295 -n 1)
-        $FUZZING_HOME/mutator/build/MutatorDriver $INPUT $SEED -v
+        $FUZZING_HOME/mutator/build_debug/MutatorDriver $INPUT $SEED -v
         if [[ $? -ne 0 ]]
         then
             echo $SEED &>> O$J

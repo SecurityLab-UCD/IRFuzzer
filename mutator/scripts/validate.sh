7c880cc2934f1d79e9a05458784622c8ea080526
Peter Rong <PeterRong96@gmail.com>
2024-03-01 12:55:39 -0800

    update scripts
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>

--- mutator/scripts/validate.sh
+++ mutator/scripts/validate.sh
@@ -1,5 +1,6 @@
 INPUT=$1
 NPROC=$2
+JOB_PER_CORE=$3
 
 if [[ -z $INPUT ]]; then
     INPUT=$FUZZING_HOME/seeds/seed.bc
@@ -9,13 +10,16 @@ if [[ -z $NPROC ]]; then
     export NPROC=`nproc --all`
 fi
 
+if [[ -z $JOB_PER_CORE ]]; then
+    export JOB_PER_CORE=1000
+fi
+
 for J in $(seq $NPROC)
 do
     rm O$J; touch O$J
-    for I in $(seq 1000)
+    for SEED in $(shuf -i 0-4294967295 -n $JOB_PER_CORE)
     do     
-        SEED=$(shuf -i 0-4294967295 -n 1)
-        $FUZZING_HOME/mutator/build_debug/MutatorDriver $INPUT $SEED -v
+        $FUZZING_HOME/mutator/build-debug/MutatorDriver $INPUT $SEED -v
         if [[ $? -ne 0 ]]
         then
             echo $SEED &>> O$J
